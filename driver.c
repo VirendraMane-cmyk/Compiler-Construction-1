9b04ce8545dd08b7a364b6207b207e9ca421b4ae
VirendraMane-cmyk <117654543+VirendraMane-cmyk@users.noreply.github.com>
2025-03-11 17:23:05 +0530

    TTT

--- driver.c
+++ driver.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <time.h>
 #include "lexer.h"
-#include "parser.h"
+//#include "parser.h"
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
@@ -20,9 +20,9 @@ int main(int argc, char* argv[]) {
         printf("0. Exit\n");
         printf("1. Remove comments\n");
         printf("2. Print token list\n");
-        printf("3. Parse code and print parse tree\n");
-        printf("4. Measure execution time\n");
-        printf("Enter your choice: ");
+        // printf("3. Parse code and print parse tree\n");
+        // printf("4. Measure execution time\n");
+        // printf("Enter your choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
@@ -41,8 +41,8 @@ int main(int argc, char* argv[]) {
                     break;
                 }
             
-                twinBuffer B;
-                getStream(fp);
+                twinBuffer* B;
+                getStream(fp,B);
             
                 tokenInfo token;
                 printf("Token List:\n");
@@ -57,68 +57,68 @@ int main(int argc, char* argv[]) {
                 fclose(fp);
                 break;
             
-            case 3:
-                // Initialize the parser and lexer
-                int fd = open(argv[1], O_RDONLY);
-                ParsingTable* pTable = initialiseParsingTable();
-                FirstAndFollow* fafl = computeFirstAndFollowSets(extractGrammar());
-                createParseTable(fafl, pTable);
+            // case 3:
+            //     // Initialize the parser and lexer
+            //     int fd = open(argv[1], O_RDONLY);
+            //     ParsingTable* pTable = initialiseParsingTable();
+            //     FirstAndFollow* fafl = computeFirstAndFollowSets(extractGrammar());
+            //     createParseTable(fafl, pTable);
             
-                // Parse the input source code
-                ParseTree* pt = parseInputSourceCode(argv[1], pTable, fafl);
+            //     // Parse the input source code
+            //     ParseTree* pt = parseInputSourceCode(argv[1], pTable, fafl);
             
-                // Print the parse tree to the specified output file
-                FILE* outputFile = fopen(argv[2], "w");
-                if (outputFile == NULL) {
-                    printf("Error opening output file %s\n", argv[2]);
-                    break;
-                }
+            //     // Print the parse tree to the specified output file
+            //     FILE* outputFile = fopen(argv[2], "w");
+            //     if (outputFile == NULL) {
+            //         printf("Error opening output file %s\n", argv[2]);
+            //         break;
+            //     }
             
-                // Assuming you have a function to print the parse tree
-                printParseTree(pt, outputFile);
+            //     // Assuming you have a function to print the parse tree
+            //     printParseTree(pt, outputFile);
             
-                fclose(outputFile);
+            //     fclose(outputFile);
             
-                // Check for errors
-                if (lexicalErrorFlag || syntaxErrorFlag) {
-                    printf("Errors encountered during parsing.\n");
-                } else {
-                    printf("Parsing successful.\n");
-                }
+            //     // Check for errors
+            //     if (lexicalErrorFlag || syntaxErrorFlag) {
+            //         printf("Errors encountered during parsing.\n");
+            //     } else {
+            //         printf("Parsing successful.\n");
+            //     }
             
-                break;
+            //     break;
             
-                break;
-            case 4:
-                clock_t start_time = clock();
+            //     break;
+            // case 4:
+            //     clock_t start_time = clock();
             
-                // Call the parser function here (assuming it's named parseInputSourceCode)
-                // This will invoke both lexer and parser.
-                FILE* fp = fopen(argv[1], "r");
-                if (fp == NULL) {
-                    printf("Could not open file %s\n", argv[1]);
-                    break;
-                }
+            //     // Call the parser function here (assuming it's named parseInputSourceCode)
+            //     // This will invoke both lexer and parser.
+            //     FILE* fp = fopen(argv[1], "r");
+            //     if (fp == NULL) {
+            //         printf("Could not open file %s\n", argv[1]);
+            //         break;
+            //     }
             
-                twinBuffer B;
-                getStream(fp);
+            //     twinBuffer B;
+            //     getStream(fp);
             
-                // Assuming you have a parser function named parseInputSourceCode
-                // that takes the input file and output file names as arguments.
-                // Adjust the function name and parameters according to your parser implementation.
-                parseInputSourceCode(argv[1], argv[2]);
+            //     // Assuming you have a parser function named parseInputSourceCode
+            //     // that takes the input file and output file names as arguments.
+            //     // Adjust the function name and parameters according to your parser implementation.
+            //     parseInputSourceCode(argv[1], argv[2]);
             
-                fclose(fp);
+            //     fclose(fp);
             
-                clock_t end_time = clock();
+            //     clock_t end_time = clock();
             
-                double total_CPU_time = (double)(end_time - start_time);
-                double total_CPU_time_in_seconds = total_CPU_time / CLOCKS_PER_SEC;
+            //     double total_CPU_time = (double)(end_time - start_time);
+            //     double total_CPU_time_in_seconds = total_CPU_time / CLOCKS_PER_SEC;
             
-                printf("Total CPU Time: %f\n", total_CPU_time);
-                printf("Total CPU Time in Seconds: %f\n", total_CPU_time_in_seconds);
+            //     printf("Total CPU Time: %f\n", total_CPU_time);
+            //     printf("Total CPU Time in Seconds: %f\n", total_CPU_time_in_seconds);
             
-                break;
+            //     break;
             
             default:
                 printf("Invalid choice. Please try again.\n");
